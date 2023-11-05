2599bb78f4a6fecc2ebdf1095743af1ff2178766
tkmktkhr <mukku.wordpress@gmail.com>
2023-11-05 17:41:12 +1300

    try options api

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,29 @@
-<script setup>
+<script>
 // imports
+import { useAuthUserStore } from '@/stores/AuthUserStore';
+import { mapState } from 'pinia';
 import CartWidget from './CartWidget.vue';
+
+export default {
+  components: { CartWidget },
+  computed: {
+    ...mapState(useAuthUserStore, { user: 'userName' }),
+  },
+};
 </script>
 
+<!-- with setup -->
+<!-- <script setup>
+// imports
+import CartWidget from './CartWidget.vue';
+</script> -->
+
 <template>
-  <header
-    class="flex justify-between p-6 mb-10 items-center"
-    style="background-image: url('/vite.svg')"
-  >
+  <header class="flex justify-between p-6 mb-10 items-center">
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
-    <CartWidget />
+    <div>
+      <span class="mr-5"> name: {{ user }} </span>
+      <CartWidget class="inline-block" />
+    </div>
   </header>
 </template>
