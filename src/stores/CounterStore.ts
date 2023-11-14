10501f098f0392e42351e14c1fb61f60ce5b8948
tkmktkhr <mukku.wordpress@gmail.com>
2023-11-15 07:43:21 +1300

    add pinia type definition comments

--- src/stores/CounterStore.ts
+++ src/stores/CounterStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
+// Options API
 // export const useCounterStore = defineStore('counter', {
 //   state: () => {
 //     return { count: 0 }
@@ -14,7 +15,26 @@ import { ref } from 'vue';
 //   },
 // })
 
-// Another expression
+// interface Getters {
+//   count: (state: IState) => number;
+//   isEmpty: (state: IState) => boolean;
+//   grouped: (state: IState) => TGrouped;
+//   groupCount: (state: IState) => (name: string) => number;
+//   total: (state: IState) => number;
+// }
+
+// interface Actions {
+//   addBork: (bork: string) => void;
+//   checkout: () => void;
+//   addItems: (count: number, item: IProduct) => IProduct[];
+//   clearItem: (itemName: string) => void;
+//   setItemCount: (item: IProduct, count: number) => void;
+// }
+
+// Another expression: Composition API's style
+// ref()s become state properties
+// computed()s become getters
+// function()s become actions
 export const useCounterStore = defineStore('CounterStore', () => {
   // state
   const count = ref(0);
