6833720b4c889ffbeee9401a3ec4fe3b8319cb50
tkmktkhr <mukku.wordpress@gmail.com>
2023-12-29 17:26:55 +1300

    route path for not found

--- src/routes.ts
+++ src/routes.ts
@@ -3,4 +3,5 @@ import { RouteRecordRaw } from 'vue-router';
 export const routes: Readonly<RouteRecordRaw[]> = [
   { path: '/', component: TODO },
   { path: '/about', component: TODO },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ];
