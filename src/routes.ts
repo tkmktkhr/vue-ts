c3d2fb2c61440d62cbb6d9bd1be8a44682536fd6
tkmktkhr <mukku.wordpress@gmail.com>
2023-12-30 07:22:09 +1300

    add routes login

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import { RouteRecordRaw } from 'vue-router';
+import Login from './views/Login.vue';
 
 export const routes: Readonly<RouteRecordRaw[]> = [
   { path: '/', component: TODO },
+  { path: '/login', name: 'Login', component: Login },
   { path: '/about', component: TODO },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ];
