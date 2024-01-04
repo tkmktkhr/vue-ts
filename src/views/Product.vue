f13e37e38bcc72a638ed8cc7653623d8ea71b626
tkmktkhr <mukku.wordpress@gmail.com>
2024-01-05 07:56:16 +1300

    mv product component

---
+++ src/views/Product.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import ProductsList from '@/components/ProductsList.vue';
+
+const nameUpdateLog = () => {
+  console.log('name was updated.');
+};
+const priceUpdateLog = () => {
+  console.log('price was updated.');
+};
+</script>
+
+<template>
+  <div>
+    <ProductsList
+      @updateProductName="nameUpdateLog"
+      @updateProductPrice="priceUpdateLog"
+    />
+  </div>
+</template>
+
+<style scoped></style>
