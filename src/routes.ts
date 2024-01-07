85c4183da612f9c258f3c01212f108100785a337
tkmktkhr <mukku.wordpress@gmail.com>
2024-01-08 08:04:00 +1300

    rm main, about routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,6 @@ import Login from './views/Login.vue';
 import NotFound from './views/NotFound.vue';
 
 export const routes: Readonly<RouteRecordRaw[]> = [
-  { path: '/', component: TODO },
   { path: '/login', name: 'Login', component: Login },
-  { path: '/about', component: TODO },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ];
