5605709caf68a124bcc476bfcaa59d40af7b5452
tkmktkhr <mukku.wordpress@gmail.com>
2024-01-12 07:54:52 +1300

    add index page

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { RouteRecordRaw } from 'vue-router';
 import Login from './views/Login.vue';
 import NotFound from './views/NotFound.vue';
+import Index from './views/Index.vue';
 
 export const routes: Readonly<RouteRecordRaw[]> = [
+  { path: '/', name: 'Index', component: Index },
   { path: '/login', name: 'Login', component: Login },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ];
