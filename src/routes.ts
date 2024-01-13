24826a78f93e95d218c87be35fb2fbba63009b28
tkmktkhr <mukku.wordpress@gmail.com>
2024-01-14 09:28:14 +1300

    add product page

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,11 @@ import { RouteRecordRaw } from 'vue-router';
 import Login from './views/Login.vue';
 import NotFound from './views/NotFound.vue';
 import Index from './views/Index.vue';
+import Product from './views/Product.vue';
 
 export const routes: Readonly<RouteRecordRaw[]> = [
   { path: '/', name: 'Index', component: Index },
   { path: '/login', name: 'Login', component: Login },
+  { path: '/product', name: 'Product', component: Product },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ];
