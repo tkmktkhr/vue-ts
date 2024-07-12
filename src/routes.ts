4fb27975adb1cd49c5137ff1258f2927e3819625
tkmktkhr <mukku.wordpress@gmail.com>
2024-07-12 13:46:21 +1200

    new practice sample page

--- src/routes.ts
+++ src/routes.ts
@@ -4,11 +4,13 @@ import NotFound from './views/NotFound.vue';
 import Index from './views/Index.vue';
 import Product from './views/Product.vue';
 import ProductId from './views/ProductId.vue';
+import Practice from './views/Practice.vue';
 
 export const routes: Readonly<RouteRecordRaw[]> = [
   { path: '/', name: 'Index', component: Index },
   { path: '/login', name: 'Login', component: Login },
   { path: '/products', name: 'Product', component: Product },
   { path: '/products/:id', component: ProductId },
+  { path: '/practice', name: 'Practice', component: Practice },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ];
---
+++ src/views/Practice.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h1>Practice</h1>
+    <p>Practice</p>
+  </div>
+</template>
